43408dd85d877cdee6838f8007b0ce27d0ce0a34
Isaac Levy <isaa0267@stud.kea.dk>
2024-05-28 17:13:59 +0200

    The first branch

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import { useState } from 'react';
 
 export default function App() {
+  const[markers,setMarkers]=useState([])
+  const [reigion,setRegion]=useState({
+    latitude:55,
+    longitude:12,
+    latiudeDelta:20,
+    longitudeDelta:20
+  
+  })
+
+  function addMarker(data){
+   alert("Hej champine");
+     
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+      style={styles.map}
+       region={reigion}
+       onLongPress={addMarker}
+      >
+        {markers.map(marker=>(
+          <Marker
+            coordinate={marker.coordinate}
+            key={marker.key}
+            title={marker.title}
+          />
+        ))
+          
+        }
+       
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map:{
+   width:'100',
+   height:'100%',
+
   },
 });
